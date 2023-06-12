5cd75faefba3ec1e32316b760f6cb7871c405a94
vitossss <shvab7216@gmail.com>
2023-06-13 00:21:38 +0300

    test graphQL

--- photo_backend/settings.py
+++ photo_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "ariadne_django",
     "rest_framework",
     "dumb_app",
 ]
