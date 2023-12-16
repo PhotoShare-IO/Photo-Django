d05b54292662897bc822f7154146cc04b72c26eb
vitossss <shvab7216@gmail.com>
2023-12-16 16:35:04 +0200

    add create post functionality

--- posts/urls.py
+++ posts/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from posts.views import PostsView
 
-urlpatterns = [path("posts/", PostsView.as_view(), name="posts")]
+urlpatterns = [
+    path("posts/", PostsView.as_view(), name="posts"),
+    path("post/<int:pk>", PostsView.as_view(), name="post"),
+    path("post/create", PostsView.as_view(), name="post_create"),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import views
+from rest_framework import views, status
+from django.shortcuts import get_object_or_404
 
 from posts.models import PostModel
-from posts.serializers import PostSerializer
+from posts.serializers import PostSerializer, PostCreateSerializer
 
 
 class PostsView(views.APIView):
@@ -14,7 +15,20 @@ class PostsView(views.APIView):
         tags=["posts"],
         responses={200: PostSerializer()},
     )
-    def get(self, request):
-        posts = PostModel.objects.all()
-        serializer = PostSerializer(posts, many=True)
+    def get(self, request, pk=None):
+        if pk:
+            post = get_object_or_404(PostModel, pk=pk)
+            serializer = PostSerializer(post)
+        else:
+            posts = PostModel.objects.all()
+            serializer = PostSerializer(posts, many=True)
+
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
